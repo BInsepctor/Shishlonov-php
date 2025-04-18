dc136f6fa4bf70d312c992511f62145d07845d73
Alexander Shishlonov <145193680+BInsepctor@users.noreply.github.com>
2025-04-18 21:22:16 +0300

    psr

--- index.php
+++ index.php
@@ -1,45 +1,51 @@
 <!DOCTYPE html>
 <html lang="ru">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>2л/р</title>
-    </head>
-    <body>
-        <?php
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>2л/р</title>
+</head>
+<body>
+<?php
 
-            $encode = "utf-8";
-            $firstName = 'александр';
-            $lastName = 'шишлонов';
-            $middleName = 'алексеевич';
+$encoding = 'utf-8';
+$firstName = 'александр';
+$lastName = 'шишлонов';
+$middleName = 'алексеевич';
 
-            function getFio($lastName, $firstName, $middleName, $encode){
-                $lastName = mb_strtoupper(mb_substr($lastName,0,1, $encode), $encode).mb_substr($lastName,1,mb_strlen($lastName,$encode),$encode);
-                $firstName = mb_strtoupper(mb_substr($firstName, 0, 1, $encode), $encode);
-                $middleName = mb_strtoupper(mb_substr($middleName, 0, 1, $encode), $encode);
-                echo $lastName.' '.$firstName.'.'.$middleName.".\n";
-            }
+function getFio(string $lastName, string $firstName, string $middleName, string $encoding): string
+{
+    $formattedLastName = mb_strtoupper(mb_substr($lastName, 0, 1, $encoding), $encoding)
+        . mb_substr($lastName, 1, mb_strlen($lastName, $encoding), $encoding);
+    
+    $formattedFirstName = mb_strtoupper(mb_substr($firstName, 0, 1, $encoding), $encoding);
+    $formattedMiddleName = mb_strtoupper(mb_substr($middleName, 0, 1, $encoding), $encoding);
+    
+    return $formattedLastName . ' ' . $formattedFirstName . '.' . $formattedMiddleName . '.';
+}
 
-           getFio($lastName, $firstName, $middleName, $encode);
+echo getFio($lastName, $firstName, $middleName, $encoding) . "\n";
 
-            function getSaturdays(){
-                $startDate = new DateTime("2021-01-01 Saturday");
-                $endDate = new DateTime("2022-12-31"); 
-                $interval = new DateInterval('P7D');
-                $saturdays = [];
-                foreach (new DatePeriod($startDate, $interval, $endDate) as $dat) {
-                    if ($dat->format('d') <= 20) {
-                        $saturdays[] = $dat->format('d.m.Y');
-                    }
-                }
-                return $saturdays;
-            }
+function getSaturdays(): array
+{
+    $saturdays = [];
+    $startDate = new DateTime('2021-01-01 Saturday');
+    $endDate = new DateTime('2022-12-31');
+    $interval = new DateInterval('P7D');
+    
+    foreach (new DatePeriod($startDate, $interval, $endDate) as $date) {
+        if ((int)$date->format('d') <= 20) {
+            $saturdays[] = $date->format('d.m.Y');
+        }
+    }
+    
+    return $saturdays;
+}
 
-            $saturdays = getSaturdays();
-            foreach ($saturdays as $sat){
-                echo $sat. "<br>";
-            }
+foreach (getSaturdays() as $saturday) {
+    echo $saturday . "<br>";
+}
 
-        ?>
-    </body>
+?>
+</body>
 </html>
