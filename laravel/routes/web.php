64baa1e659e180f198c1c1c40032c843a36a0981
Alexander Shishlonov <145193680+BInsepctor@users.noreply.github.com>
2025-04-23 23:19:10 +0300

    add migrate

---
+++ laravel/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/user', function() {
+//     return User::first();
+// });
+
+Route::get('/user', [UserController::class, 'show']);
